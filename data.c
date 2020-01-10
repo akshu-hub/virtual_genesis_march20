902922333ceee6849f0d7e28261ba031c02eb4fd
akshu-hub <59725327+akshu-hub@users.noreply.github.com>
2020-01-10 23:04:07 +0530

    Create data.c

---
+++ data.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ char *data[]={"Hi","Iam akshata","Iam twenty years old","AND","Iam enginear"};
+ int i;
+ printf("MYdata is:");
+ for(i=0;i<5;i++)
+ {
+  printf("\n%s",data[i]);
+ }
+return 0;
+}
