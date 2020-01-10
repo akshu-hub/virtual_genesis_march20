e42ccea9528c491a98a60c2bfb151e300570b1cf
akshu-hub <59725327+akshu-hub@users.noreply.github.com>
2020-01-10 23:28:52 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int n,fact=1,i;
+ printf("Enter any number:");
+ scanf("%d",n);
+ for(i=1;i>=n;i++)
+ {
+  fact=fact*i;
+ }
+ printf("factorial number is:%d",fact);
+ return 0;
+}
